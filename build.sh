c2d6297fbb4ac5e4148af117dc80c7d6a9ba68b6
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-07-28 18:06:13 +0200

    Add option to use xelatex to avoid problems with encoding

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-pandoc -V linkcolor:blue -f markdown documentation/* -o test.pdf && okular test.pdf
+pandoc --pdf-engine=xelatex -V linkcolor:blue -f markdown documentation/* -o test.pdf && okular test.pdf
