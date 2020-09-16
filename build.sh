ce1b59aa4112356dba1e800305670a1a8e64131d
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-09-16 17:09:51 +0100

    Change extension of template filename

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ pandoc --pdf-engine=xelatex \
   --toc --toc-depth=2 \
   --top-level-division=part \
   -V linkcolor:blue \
-   --template template.latex \
+   --template template.tex \
    -f markdown \
    documentation/title_page.md documentation/introduction.md documentation/good_dm_practice.md documentation/storing_data.md documentation/directory_structure.md documentation/file_naming.md documentation/file_formats.md documentation/backing_up_data.md documentation/working_on_data.md documentation/sample_collection.md documentation/field_guide_introduction.md documentation/planning_before_go.md documentation/in_the_field.md documentation/return_from_field.md \
    -s -o data_management_guidance_draft.tex
