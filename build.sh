3caab7e08183943cde1f4a82c47783de4415c914
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-09-16 16:23:54 +0100

    Tidy up build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Run spell checker
 find . -name "*.md" -exec aspell -d en -x --home-dir=. check {} \;
 
-#pandoc --pdf-engine=xelatex  --variable subparagraph --toc --toc-depth=2 --top-level-division=part -V linkcolor:blue -f markdown documentation/title_page.md documentation/introduction.md documentation/good_dm_practice.md documentation/storing_data.md documentation/directory_structure.md documentation/file_naming.md documentation/file_formats.md documentation/backing_up_data.md documentation/working_on_data.md documentation/sample_collection.md documentation/field_guide_introduction.md documentation/planning_before_go.md documentation/in_the_field.md documentation/return_from_field.md  -s -o data_management_guidance_draft.tex
+# Generate tex file output using a template
 pandoc --pdf-engine=xelatex \
   --variable subparagraph \
   --toc --toc-depth=2 \
@@ -16,16 +16,13 @@ pandoc --pdf-engine=xelatex \
    documentation/title_page.md documentation/introduction.md documentation/good_dm_practice.md documentation/storing_data.md documentation/directory_structure.md documentation/file_naming.md documentation/file_formats.md documentation/backing_up_data.md documentation/working_on_data.md documentation/sample_collection.md documentation/field_guide_introduction.md documentation/planning_before_go.md documentation/in_the_field.md documentation/return_from_field.md \
    -s -o data_management_guidance_draft.tex
 
-#pandoc -t docx data_management_guidance_draft.tex -o data_management_guidance_draft.docx
 pdflatex  data_management_guidance_draft.tex
 pdflatex  data_management_guidance_draft.tex
 pdflatex  data_management_guidance_draft.tex
 pdflatex  data_management_guidance_draft.tex
 
-
-# This produces nice fonts but not the correct format
-#pandoc --pdf-engine=xelatex -V 'mainfont:Times.ttf' -V 'monofont:AndaleMono' --variable subparagraph --toc --toc-depth=2 --top-level-division=part -V linkcolor:blue -f latex data_management_guidance_draft.tex -s -o data_management_guidance_draft.pdf # formatting is all to pot
-
+# Generate a docx output
+# pandoc -t docx data_management_guidance_draft.tex -o data_management_guidance_draft.docx
 
 # Example of using a reference doc to format an output docx file
 #pandoc --reference-doc='/home/jen/projects/spi_data_management/SPI_document_template.docx' -t docx data_management_guidance_draft.tex -o data_management_guidance_draft.docx
