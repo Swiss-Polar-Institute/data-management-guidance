7a9069e6c80f9081eb83161f3ebfe85c6863a626
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-10-01 21:40:56 +0100

    Remove test template file

--- test_files/build.sh
+++ test_files/build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# Not currently using this line because the table of contents page numbers are incorrectly offset by one
-#pandoc --pdf-engine=xelatex --variable subparagraph --toc --toc-depth=2 --top-level-division=part -V linkcolor:blue -f markdown documentation/title_page.md documentation/introduction.md documentation/good_dm_practice.md documentation/storing_data.md documentation/directory_structure.md documentation/file_naming.md documentation/file_formats.md documentation/backing_up_data.md documentation/working_on_data.md documentation/sample_collection.md documentation/planning_before_go.md documentation/in_the_field.md documentation/return_from_field.md   -o data_management_guidance_draft.pdf && okular data_management_guidance_draft.pdf
 
 # Run spell checker
 find . -name "*.md" -exec aspell -d en -x --home-dir=. check {} \;
@@ -14,7 +12,7 @@ pandoc --pdf-engine=xelatex \
   -V linkcolor:blue \
    --template template.tex \
    -f markdown \
-   documentation/title_page.md documentation/introduction.md documentation/good_dm_practice.md documentation/storing_data.md documentation/directory_structure.md documentation/file_naming.md documentation/file_formats.md documentation/backing_up_data.md documentation/working_on_data.md documentation/sample_collection.md documentation/field_guide_introduction.md documentation/planning_before_go.md documentation/in_the_field.md documentation/return_from_field.md \
+   ../documentation/title_page.md ../documentation/introduction.md ../documentation/good_dm_practice.md
    -s -o data_management_guidance_draft.tex
 
 pdflatex  data_management_guidance_draft.tex
@@ -22,8 +20,3 @@ pdflatex  data_management_guidance_draft.tex
 pdflatex  data_management_guidance_draft.tex
 pdflatex  data_management_guidance_draft.tex
 
-# Generate a docx output
-pandoc -t docx data_management_guidance_draft.tex -o data_management_guidance_draft.docx
-
-# Example of using a reference doc to format an output docx file
-#pandoc --reference-doc='/home/jen/projects/spi_data_management/SPI_document_template.docx' -t docx data_management_guidance_draft.tex -o data_management_guidance_draft.docx
