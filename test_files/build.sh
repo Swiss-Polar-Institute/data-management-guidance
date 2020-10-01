e1e27ec86f87174a2e497e309a0381686b50da25
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-10-01 21:41:39 +0100

    Add draft tex file to change for test

--- test_files/build.sh
+++ test_files/build.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-# Run spell checker
-find . -name "*.md" -exec aspell -d en -x --home-dir=. check {} \;
-
-# Generate tex file output using a template
-pandoc --pdf-engine=xelatex \
-  --variable subparagraph \
-  --toc --toc-depth=2 \
-  -V fontsize=12pt \
-  --top-level-division=part \
-  -V linkcolor:blue \
-   --template template.tex \
-   -f markdown \
-   ../documentation/title_page.md ../documentation/introduction.md ../documentation/good_dm_practice.md
-   -s -o data_management_guidance_draft.tex
-
 pdflatex  data_management_guidance_draft.tex
 pdflatex  data_management_guidance_draft.tex
 pdflatex  data_management_guidance_draft.tex
