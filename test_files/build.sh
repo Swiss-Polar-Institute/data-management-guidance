c5d54afb694a502679f399b9d531319ba035e5e6
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-10-01 23:31:03 +0100

    Remove all test files and those files from .gitignore

--- test_files/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pdflatex  data_management_guidance_draft.tex
-pdflatex  data_management_guidance_draft.tex
-pdflatex  data_management_guidance_draft.tex
-pdflatex  data_management_guidance_draft.tex
-
