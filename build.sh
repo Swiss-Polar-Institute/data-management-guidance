24e182b678715c8ace27a95b5a3256ffe5abf0e9
Carles Pina i Estany <carles@pina.cat>
2020-10-21 09:39:03 +0100

    Add support for variables and use it for Git commit

--- build.sh
+++ build.sh
@@ -3,7 +3,16 @@
 #pandoc --pdf-engine=xelatex --variable subparagraph --toc --toc-depth=2 --top-level-division=part -V linkcolor:blue -f markdown documentation/title_page.md documentation/introduction.md documentation/good_dm_practice.md documentation/storing_data.md documentation/directory_structure.md documentation/file_naming.md documentation/file_formats.md documentation/backing_up_data.md documentation/working_on_data.md documentation/sample_collection.md documentation/planning_before_go.md documentation/in_the_field.md documentation/return_from_field.md   -o data_management_guidance_draft.pdf && okular data_management_guidance_draft.pdf
 
 # Run spell checker
-find . -name "*.md" -exec aspell -d en -x --home-dir=. check {} \;
+find documentation/ -name "*.md" -exec aspell -d en -x --home-dir=. check {} \;
+
+echo "git_commit: $(git log -n 1 --pretty=format:'%h')" >> variables.yml
+
+mkdir -p rendered
+for file in documentation/*
+do 
+	file_basename=$(basename "$file")
+	mustache variables.yml "$file" > "rendered/$file_basename"
+done
 
 # Generate tex file output using a template
 pandoc --pdf-engine=xelatex \
@@ -18,7 +27,7 @@ pandoc --pdf-engine=xelatex \
   -V linkcolor:blue \
    --template template.tex \
    -f markdown \
-   documentation/title_page.md documentation/introduction.md documentation/good_dm_practice.md documentation/metadata.md documentation/storing_data.md documentation/directory_structure.md documentation/file_naming.md documentation/file_formats.md documentation/backing_up_data.md documentation/working_on_data.md documentation/sample_collection.md documentation/field_guide_introduction.md documentation/planning_before_go.md documentation/in_the_field.md documentation/return_from_field.md documentation/bibliography.md \
+   rendered/title_page.md rendered/introduction.md rendered/good_dm_practice.md rendered/metadata.md rendered/storing_data.md rendered/directory_structure.md rendered/file_naming.md rendered/file_formats.md rendered/backing_up_data.md rendered/working_on_data.md rendered/sample_collection.md rendered/field_guide_introduction.md rendered/planning_before_go.md rendered/in_the_field.md rendered/return_from_field.md rendered/bibliography.md \
    -s -o data_management_guidance_draft.tex
 
 pdflatex  data_management_guidance_draft.tex
--- documentation/bibliography.md
+++ documentation/bibliography.md
@@ -6,3 +6,5 @@
 nocite: | 
   @wikipediaSha1sum, @zenodoZenodoResearchShared, @cookBestPracticesPreparing2001, @githubinc.GitHubWhereWorld2020, @greenbeltmd:earthsciencedataandinformationsystemearthscienceprojectsdivisiongoddardspaceflightcentergsfcnationalaeronauticsandspaceadministrationnasaGlobalChangeMaster2020, @swissdatasciencecenterRenku, @theuniversityofedinburghStorageSecurity, @wholetaleWholeTale2019, @wikipediacontributorsMd5sum2020, @wikipediacontributorsChecksum2020, @wikipediacontributorsSha1sum2020, @wikipediacontributorsComparisonOnlineBackup2020, @CFConventionsHome, @FrictionlessData, @SchemaOrg, SeaDataNet
 ...
+
+Git commit: {{git_commit}}
