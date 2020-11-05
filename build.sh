662bbe4f4c6ada2e1ca7144cb12958f3d684a22b
Carles Pina i Estany <carles@pina.cat>
2020-11-05 10:13:59 +0000

    Use set -e in build.sh to stop the execution if any command fails

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e  # if a command fails it stops the execution
+set -u  # script fails if trying to access to an undefined variable
+
 # Run spell checker
 find documentation/ -name "*.md" -exec aspell -d en -x --home-dir=. check {} \;
 
@@ -28,11 +31,11 @@ pandoc --pdf-engine=xelatex \
    --template template.tex \
    -f markdown \
    rendered/title_page.md rendered/introduction.md rendered/good_dm_practice.md rendered/metadata.md rendered/storing_data.md rendered/file_organisation.md rendered/file_formats.md rendered/backing_up_data.md rendered/working_on_data.md rendered/sample_collection.md rendered/field_guide_introduction.md rendered/planning_before_go.md rendered/in_the_field.md rendered/return_from_field.md rendered/bibliography.md \
-   -s -o data_management_guidance_draft.tex && \
-pdflatex  data_management_guidance_draft.tex && \
-pdflatex  data_management_guidance_draft.tex && \
-pdflatex  data_management_guidance_draft.tex && \
-pdflatex  data_management_guidance_draft.tex && \
+   -s -o data_management_guidance_draft.tex
+pdflatex  data_management_guidance_draft.tex
+pdflatex  data_management_guidance_draft.tex
+pdflatex  data_management_guidance_draft.tex
+pdflatex  data_management_guidance_draft.tex
 
 # Generate a docx output
 pandoc -t docx data_management_guidance_draft.tex -o data_management_guidance_draft.docx
